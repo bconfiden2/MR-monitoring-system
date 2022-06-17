7525c59c5f465ef27250c45387e578a532ae4660
bconfiden2 <bconfiden2@naver.com>
2022-06-17 13:22:52 +0900

    [#1] 클러스터 설정값 세팅 / 가져오기

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, Blueprint, request
 from flask.templating import render_template
-
+import json
 
 bp = Blueprint('main', __name__, url_prefix='/')
+configs = json.load(open("cluster.conf", "r"))
+ResourceManager = configs["RM-IP:Port"] if "RM-IP:Port" in configs else "localhost:8088"
+ApplicationMaster = configs["AM-IP:Port"] if "AM-IP:Port" in configs else "localhost:8088"
 
 @bp.route('/')
 def index():
