c7282b823e19bb52ca4e3dd32fcdf4213475bad5
bconfiden2 <bconfiden2@naver.com>
2022-06-18 10:37:27 +0900

    [#3] head에 meta 태그 사용하여 특정 주기마다 다시 요청하게

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ from utils import *
 import json
 
 bp = Blueprint('main', __name__, url_prefix='/')
-configs = json.load(open("cluster.conf", "r"))
+configs = json.load(open("setting.conf", "r"))
 ResourceManager = configs["RM-IP:Port"] if "RM-IP:Port" in configs else "localhost:8088"
 ApplicationMaster = configs["AM-IP:Port"] if "AM-IP:Port" in configs else "localhost:8088"
+interval = configs["update-interval"] if "update-interval" in configs else "5"
+
 
 @bp.route('/')
 def index():
     app_list = get_apps(ResourceManager, "RUNNING")
-    
-    data = {"apps": app_list, "AM": ApplicationMaster}
+    print("!!") 
+    data = {"apps": app_list, "AM": ApplicationMaster, "interval": interval}
     return render_template('index.html', data=data)
 
 
