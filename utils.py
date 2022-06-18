36116cecd26714462aefcc1be70e42dfc3641606
bconfiden2 <bconfiden2@naver.com>
2022-06-18 10:39:10 +0900

    [#2] 애플리케이션마스터에서 태스크들 상태정보 가져오기

--- utils.py
+++ utils.py
@@ -30,3 +30,34 @@ def get_apps(RM, status):
         apps.append((app_id, app_name))
 
     return apps
+
+def get_tasks(AM, app_id, task_flg):
+    if task_flg not in ('m','r'):
+        return []
+    response = requests.get(f"http://{AM}/proxy/application_{app_id}/mapreduce/attempts/job_{app_id}/{task_flg}/RUNNING")
+    result = convert_response(response)
+
+    tasks = []
+    flg = False
+    for line in result.split('\n'):
+        if line.strip() == "var attemptsTableData=[":
+            flg = True
+            continue
+
+        if not flg:
+            continue
+
+        if line[0] == ']':
+            flg = False
+            continue
+
+        line = line[1:-1].split(',')
+        line[0] = '-'.join(line[0].split('>')[1].split('<')[0].split('_')[4:])
+        line[1] = float(line[1][1:-1])
+        line[2] = line[2]
+        line[3] = "status"
+        line[4] = line[4].split('>')[1].split('<')[0].split(':')[0]
+    
+        tasks.append(line[:5])
+
+    return tasks
