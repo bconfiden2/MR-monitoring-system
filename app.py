c90a7be66a75115e1bf06cff66f0f9dc29266356
bconfiden2 <bconfiden2@naver.com>
2022-06-22 15:33:28 +0900

    [#10] target_app_id 를 파라미터로 받는 GET URL 추가

--- app.py
+++ app.py
@@ -33,6 +33,28 @@ def index():
     data = {"workers": workers, "apps": app_list, "mappers": mappers, "reducers": reducers, "interval": interval}
     return render_template('index.html', data=data)
 
+@bp.route('/<target_app_id>')
+def application(target_app_id):
+    app_list = get_apps(ResourceManager, "RUNNING")
+
+    tasks = [[], []]
+    for app_id, _ in app_list:
+        if app_id == target_app_id:
+            tasks = get_tasks(ApplicationMaster, app_id)
+            break
+
+    map_tasks = {node:[] for node in workers}
+    for task in tasks[0]:
+        map_tasks[task[4]].append(int(task[1]))
+    mappers = [sorted(map_tasks[node], reverse=True) for node in workers]
+
+    reduce_tasks = {node:[] for node in workers}
+    for task in tasks[1]:
+        reduce_tasks[task[4]].append(int(task[1]))
+    reducers = [sorted(reduce_tasks[node], reverse=True) for node in workers]
+
+    data = {"workers": workers, "apps": app_list, "mappers": mappers, "reducers": reducers, "interval": interval}
+    return render_template('index.html', data=data)
 
 app = Flask(__name__)
 app.register_blueprint(bp)
