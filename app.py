6adab1021dcfe49d7f285ff05dcbd920a896c2fb
bconfiden2 <bconfiden2@naver.com>
2022-06-23 11:39:41 +0900

    [#10] 코드 중복 제거

--- app.py
+++ app.py
@@ -15,34 +15,22 @@ with open(worker_file, "r") as f:
 @bp.route('/')
 def index():
     app_list = get_apps(ResourceManager, "RUNNING")
-    
-    tasks = [[], []]
     if len(app_list) > 0:
-        tasks = get_tasks(ApplicationMaster, app_list[0][0])
-    
-    map_tasks = {node:[] for node in workers}
-    for task in tasks[0]:
-        map_tasks[task[4]].append(int(task[1]))
-    mappers = [sorted(map_tasks[node], reverse=True) for node in workers]
+        return application(app_list[0][0])
 
-    reduce_tasks = {node:[] for node in workers}
-    for task in tasks[1]:
-        reduce_tasks[task[4]].append(int(task[1]))
-    reducers = [sorted(reduce_tasks[node], reverse=True) for node in workers]
-
-    data = {"workers": workers, "apps": app_list, "mappers": mappers, "reducers": reducers, "interval": interval}
+    data = {"workers": workers, "apps": [], "mappers": [[] for node in workers], "reducers": [[] for node in workers], "interval": interval}
     return render_template('index.html', data=data)
 
 @bp.route('/<target_app_id>')
 def application(target_app_id):
     app_list = get_apps(ResourceManager, "RUNNING")
-
+    
     tasks = [[], []]
     for app_id, _ in app_list:
         if app_id == target_app_id:
             tasks = get_tasks(ApplicationMaster, app_id)
             break
-
+            
     map_tasks = {node:[] for node in workers}
     for task in tasks[0]:
         map_tasks[task[4]].append(int(task[1]))
@@ -55,6 +43,7 @@ def application(target_app_id):
 
     data = {"workers": workers, "apps": app_list, "mappers": mappers, "reducers": reducers, "interval": interval}
     return render_template('index.html', data=data)
+
 
 app = Flask(__name__)
 app.register_blueprint(bp)
